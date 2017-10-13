8b045ec66b9caba667178b28c001493ae9e2103b
hvdwolf <hvdwolf@gmail.com>
2017-10-13 19:39:58 +0200

    update install files

--- seSuperuser/install.sh
+++ seSuperuser/install.sh
@@ -5,7 +5,7 @@
 if [ "$1" = "" ]
 then
         echo "Provide IP Address to connect to for adb"
-        echo "for example:  ./copy_install.sh 192.168.178.52"
+        echo "for example:  ./install.sh 192.168.178.52"
         exit
 fi
 set -x
@@ -22,12 +22,13 @@ sleep 2
 # Make the partitions read-writable
 adb shell mount -o rw,remount /system
 
-REM copy necessary files
+# copy necessary files
 adb shell "mkdir -p /data/seSuperuser"
 adb push su /data/seSuperuser
 adb push jy-setup.sh /data/seSuperuser
+adb push Superuser.apk /data/seSuperuser
 
-REM Do the actual installation
+# Do the actual installation
 adb shell chmod 0755 /data/seSuperuser/jy-setup.sh
 adb shell "cd /data/seSuperuser && sh jy-setup.sh"
 
@@ -36,7 +37,7 @@ adb shell rm -rf /data/seSuperuser
 adb shell sync
 #adb shell mount -o ro,remount /system
 
-echo Reboot your Head Unit NOW!
-echo After the reboot go to the play store and download "phh's SuperUser"
-echo or simply go to https://play.google.com/store/apps/details?id=me.phh.superuser
+echo "Reboot your Head Unit NOW!"
+echo 'After the reboot go to the play store and download "phh\'s SuperUser"'
+echo "or simply go to https://play.google.com/store/apps/details?id=me.phh.superuser"
 
--- seSuperuser/jy-setup.sh
+++ seSuperuser/jy-setup.sh
@@ -9,12 +9,17 @@
 # Version 1.0, 28 September 2017, HvdW
 
 # Make backup of original su
-cp -f /system/bin/su /system/bin/su.org
+cp -f /system/xbin/su /system/xbin/su.org
 cp -f /data/seSuperuser/su /system/bin/su
-chmod 06775 /system/bin/su
+chmod 0775 /system/bin/su
+
+mkdir -p /system/app/Superuser
+chmod 755 /system/app/Superuser
+cp /data/Superuser.apk /system/app/Superuser/Superuser.apk
+chmod 644 /system/app/Superuser/Superuser.apk 
 
 # Is there already an install-recovery.sh ?
-if [ ! -f /system/bin/install-recovery.sh ]
+if [ ! -e /system/bin/install-recovery.sh ]
 then
 	cp -f /system/bin/install-recovery.sh /system/bin/install-recovery.sh.org
 fi
@@ -24,7 +29,7 @@ printf '#!/bin/system/sh\n' > /system/bin/install-recovery.sh
 printf '\n\n' >> /system/bin/install-recovery.sh
 printf '# This /system/bin/install-recovery.sh is installed here to start the\n' >> /system/bin/install-recovery.sh
 printf '# seSuperuser su binary in daemon mode\n\n' >> /system/bin/install-recovery.sh
-printf 'su --daemon &\n' >> /system/bin/install-recovery.sh
+printf '/system/bin/su --daemon &\n' >> /system/bin/install-recovery.sh
 chmod 755 /system/bin/install-recovery.sh
 
 
